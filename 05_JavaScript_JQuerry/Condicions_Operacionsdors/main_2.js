0c463e4a31a5ad8bec37d6a15bee00ef78749852
Bernat Ribas Romeu <bernat162@gmail.com>
2024-09-26 12:52:10 +0200

    Exercicis condicions i operadors

---
+++ 05_JavaScript_JQuerry/Condicions_Operacionsdors/main_2.js
@@ -0,0 +1,12 @@
+// * Exercici 1: Creem una funció que sumi 3 numeros donats per paràmetre i ens doni el resultat amb un return que s'imrimirà fora de la funció
+console.log("Exercici 1: Suma de 3 numeros")
+function suma (num1, num2, num3){
+    resultat = num1 + num2 + num3;
+    return resultat;
+}
+let num1 = 4;
+let num2 = 6;
+let num3 = 3;
+console.log(num1 + " + " + num2 + " + " + num3);
+suma(num1, num2, num3);
+console.log(resultat);
