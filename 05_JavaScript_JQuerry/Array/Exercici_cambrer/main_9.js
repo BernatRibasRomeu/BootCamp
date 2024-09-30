443da2dd14bd7ba22d2058b06e9a63f6b08ee2e9
Bernat Ribas Romeu <bernat162@gmail.com>
2024-09-30 13:57:21 +0200

    Seguim amb els arrays de notion

---
+++ 05_JavaScript_JQuerry/Array/Exercici_cambrer/main_9.js
@@ -0,0 +1,5 @@
+// Hem d'ordenar el seguent array de diferents maneres
+// Primer fem servir el mètode sort
+const tickets = [1, 100000, 21, 30, 4]
+console.log(tickets.sort((a, b) => a - b));
+// Ara ho fem amb un bucle for sense mètodes de programacio funcional
