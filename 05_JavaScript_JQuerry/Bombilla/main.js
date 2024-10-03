bd27a5c8292bc85a890adb4d0aa1b505c07af084
Bernat Ribas Romeu <bernat162@gmail.com>
2024-10-03 12:13:32 +0200

    Tot el que hem fet amb la bombeta funciona, falten coses noves

--- 05_JavaScript_JQuerry/Bombilla/main.js
+++ 05_JavaScript_JQuerry/Bombilla/main.js
@@ -1,13 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
+    var bulb = document.querySelectorAll('.bulb');
     var bulbOn = document.querySelector('.bulb-on');
     var bulbOff = document.querySelector('.bulb-off');
     var switchOn = document.querySelector('.switch-on');
     var switchOff = document.querySelector('.switch-off');
     var intensityInput = document.getElementById('intensity');
+    var sizeInput = document.getElementById('size');
+    var isTurnedOn = false;
     bulbOn.style.display = 'none';
     bulbOff.style.display = 'block';
     switchOn.style.display = 'none';
     var turnOn = function () {
+        isTurnedOn = true;
         bulbOff.style.display = 'none';
         bulbOn.style.display = 'block';
         switchOff.style.display = 'none';
@@ -15,6 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
         adjustIntensity();
     };
     var turnOff = function () {
+        isTurnedOn = false;
         bulbOn.style.display = 'none';
         bulbOff.style.display = 'block';
         switchOn.style.display = 'none';
@@ -30,10 +35,32 @@ document.addEventListener("DOMContentLoaded", function () {
         bulbOff.style.opacity = (1 - (intensityValue / 100)).toString();
     };
     var disableIntensity = function () {
-        intensityInput.value = '0'; // O cualquier lógica que desees implementar
-        bulbOff.style.display = 'block';
+        if (isTurnedOn) {
+            bulbOff.style.opacity = '0';
+            bulbOff.style.display = 'block';
+            var intensityValue = Number(intensityInput.value);
+            bulbOn.style.opacity = (intensityValue / 100).toString();
+            bulbOff.style.opacity = (1 - (intensityValue / 100)).toString();
+        }
+        else {
+            bulbOff.style.opacity = '100';
+            bulbOff.style.display = 'block';
+        }
+    };
+    var adjustSize = function () {
+        var sizeValue = Number(sizeInput.value);
+        bulb.forEach(function (element) {
+            if (sizeValue <= 100) {
+                var minSize = 100;
+                element.style.width = "".concat((minSize.toString()), "px");
+            }
+            else {
+                element.style.width = "".concat((sizeValue.toString()), "px");
+            }
+        });
     };
     switchOff.addEventListener('click', turnOn);
     switchOn.addEventListener('click', turnOff);
     intensityInput.addEventListener('input', adjustIntensity);
+    sizeInput.addEventListener('input', adjustSize);
 });
--- 05_JavaScript_JQuerry/Bombilla/main.ts
+++ 05_JavaScript_JQuerry/Bombilla/main.ts
@@ -1,23 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const bulb = document.querySelectorAll('.bulb') as NodeListOf<HTMLElement>;
     const bulbOn = document.querySelector('.bulb-on') as HTMLImageElement;
     const bulbOff = document.querySelector('.bulb-off') as HTMLImageElement;
     const switchOn = document.querySelector('.switch-on') as HTMLImageElement;
     const switchOff = document.querySelector('.switch-off') as HTMLImageElement;
     const intensityInput = document.getElementById('intensity') as HTMLInputElement;
+    const sizeInput = document.getElementById('size') as HTMLInputElement;
+
+    let isTurnedOn = false;
 
     bulbOn.style.display = 'none';
     bulbOff.style.display = 'block'
     switchOn.style.display = 'none';
 
     const turnOn = () => {
+        isTurnedOn = true;
         bulbOff.style.display = 'none';
         bulbOn.style.display = 'block';
         switchOff.style.display = 'none';
         switchOn.style.display = 'block';
         adjustIntensity();
     };
-    
+
     const turnOff = () => {
+        isTurnedOn = false;
         bulbOn.style.display = 'none';
         bulbOff.style.display = 'block';
         switchOn.style.display = 'none';
@@ -35,8 +41,28 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     const disableIntensity = () => {
-        intensityInput.value = '0'; // O cualquier lógica que desees implementar
-        bulbOff.style.display = 'block';
+        if (isTurnedOn) {
+            bulbOff.style.opacity = '0';
+            bulbOff.style.display = 'block';
+            const intensityValue = Number(intensityInput.value);
+            bulbOn.style.opacity = (intensityValue / 100).toString();
+            bulbOff.style.opacity = (1 - (intensityValue / 100)).toString();
+        } else {
+            bulbOff.style.opacity ='100';
+            bulbOff.style.display = 'block';
+        }
+    };
+
+    const adjustSize = () => {
+        const sizeValue = Number(sizeInput.value);
+        bulb.forEach(element => {
+            if (sizeValue <= 100) {
+                let minSize = 100;
+                element.style.width = `${(minSize.toString())}px`
+            } else {
+                element.style.width = `${(sizeValue.toString())}px`;
+            }
+        });
     };
 
     switchOff.addEventListener('click', turnOn);
@@ -44,4 +70,6 @@ document.addEventListener("DOMContentLoaded", () => {
     switchOn.addEventListener('click', turnOff);
 
     intensityInput.addEventListener('input', adjustIntensity);
+
+    sizeInput.addEventListener('input', adjustSize);
 });
