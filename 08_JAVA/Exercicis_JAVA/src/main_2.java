5937a975760c5e4251ddf91449b8b0cce5b76182
Bernat Ribas Romeu <bernat162@gmail.com>
2024-10-25 11:46:37 +0200

    Exercicis JAVA

---
+++ 08_JAVA/Exercicis_JAVA/src/main_2.java
@@ -0,0 +1,8 @@
+public class main_2 {
+    public static void main(String[] args) {
+        int num1 = 10; // Iniciem variable i la declarem
+        int num2 = 20; // Iniciem variable i la declarem
+        int suma = num1 + num2; // Iniciem variable i manipulem les dades de altres variabels
+        System.out.println(suma); // Mostrem per pantalla el resultat indicat
+    }
+}
---
+++ 08_JAVA/Exercicis_JAVA/src/main_4.java
@@ -0,0 +1,12 @@
+public class main_4 {
+    public static void main(String[] args) {
+        byte num1 = (byte) 132;
+        System.out.println(num1);
+        short num2 = (short) 32792;
+        System.out.println(num2);
+        int num3 = (int) 2147493647L;
+        System.out.println(num3);
+        long num4 = (long) 9223372036854795807f;
+        System.out.println(num4);
+    }
+}
