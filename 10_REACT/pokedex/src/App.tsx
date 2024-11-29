bf5eca524f9803af9d05e31dfc3e3b42d530f066
Bernat Ribas Romeu <bernat162@gmail.com>
2024-11-29 12:17:49 +0100

    Em creat un paginat per poder veure tots els pokemon del 1 al 151, els originals, i hem afegit un placeholder basic per la carga de les pagines

--- 10_REACT/pokedex/src/App.tsx
+++ 10_REACT/pokedex/src/App.tsx
@@ -1,33 +1,18 @@
-import React, { useState, useEffect } from "react";
-import PokemonCard from "./components/PokemonCard";
+import React from "react";
 import PokemonSearch from "./components/PokemonSearch";
+import Pagination from "./components/Pagination";
 
 const App: React.FC = () => {
-  const [pokemonIds, setPokemonIds] = useState<number[]>([]); // Estado que contiene un array de números.
-
-  useEffect(() => {
-    const generateRandomIds = (): number[] => {
-      const ids = new Set<number>();
-      while (ids.size < 20) {
-        ids.add(Math.floor(Math.random() * 151) + 1);
-      }
-      return Array.from(ids);
-    };
-
-    setPokemonIds(generateRandomIds());
-  }, []);
 
   return (
     <>
-    <h1 className="text-center text-2xl font-bold text-purple-800">Pokedex amb React</h1>
+      <h1 className="text-center text-2xl font-bold text-purple-800">Pokedex amb React</h1>
       <div className="flex items-center justify-center">
-      <PokemonSearch />
-    </div>
-      <div className="grid grid-cols-5 gap-4 p-4 max-w-4xl mx-auto">
-        {pokemonIds.map((id) => (
-          <PokemonCard key={id} pokemonId={id} />
-        ))}
+        <PokemonSearch />
       </div>
+      <div>
+      <Pagination totalPages={8} />
+    </div>
     </>
   );
 };
